31c8077c72c2383a3c33dd6068d7f1ef5632fc75
mohamedeliwa <mohamed.av11@gmail.com>
2020-03-23 13:26:09 +0200

    Init commit

---
+++ server.js
@@ -0,0 +1,45 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+// var cors = require('cors');
+// app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+ 
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    /**
+     * getting the ip address from request headers
+     * I can get it from `req.ip` too
+     * for local development use `req.ip` instead as
+     * `req.headers["x-forwarded-for"]` will not work
+     *  */ 
+    "ipaddress": req.ip, //req.headers["x-forwarded-for"].split(",")[0]
+    // getting the preferred languages 
+    "language":req.headers["accept-language"],
+    // getting system infos for user device 
+    "software":req.headers["user-agent"]
+  });
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
